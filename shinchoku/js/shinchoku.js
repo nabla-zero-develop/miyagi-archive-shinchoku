9f9e390d07dda00010f55d1d910159e225d2f725
Hideo Ohashi <hideld@infoseek.jp>
2014-06-15 19:15:58 +0900

    カレンダーを選択したときにデータを取得し、表示する

--- shinchoku/js/shinchoku.js
+++ shinchoku/js/shinchoku.js
@@ -1,10 +1,46 @@
 $(function () {
     "use strict";
 
+    function updateShinchokuTable(data, table) {
+        var records = [];
+
+        $.each(data, function (key, value) {
+            var row = "<tr>";
+
+            for (var name in value) {
+                row += "<td>" + value[name] + "</td>";
+            }
+
+            row += "</tr>";
+
+            records.push(row);
+        });
+
+        table.children("tbody").empty().append(records.join(""));
+
+        table.trigger("update");
+    }
+
+    function onSelectDate(date) {
+        var table = $(this).parent().children(".tablesorter");
+
+        $.getJSON(
+            "shinchoku_ajax.php",
+            {
+                categoryid: {department: 1, municipalities: 2, digital_team: 3}[table.parent().attr("id")],
+                date: date
+            },
+            function (data) {
+                updateShinchokuTable(data, table);
+            }
+        );
+    }
+
     $(".tabs").tabs();
 
     $(".datepicker").datepicker({
-        dateFormat: "yy-mm-dd"
+        dateFormat: "yy-mm-dd",
+        onSelect: onSelectDate
     });
 
     $(".tablesorter").tablesorter();
