cb0cddf22612228baed59112c9583a4004e522f5
Hideo Ohashi <hideld@infoseek.jp>
2014-06-15 14:45:31 +0900

    進捗管理用データベース作成スクリプトを追加

---
+++ sql/create_shinchoku_database.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE  IF NOT EXISTS `miyagi_archive_shinchoku` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `miyagi_archive_shinchoku`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+--
+-- Host: 127.0.0.1    Database: miyagi_archive_shinchoku
+-- ------------------------------------------------------
+-- Server version	5.5.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `daily_shinchoku`
+--
+
+DROP TABLE IF EXISTS `daily_shinchoku`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `daily_shinchoku` (
+  `shincyoku_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `categoryid` int(11) NOT NULL,
+  `holderid` bigint(20) NOT NULL,
+  `content_num` bigint(20) NOT NULL,
+  `copyright_num` bigint(20) NOT NULL,
+  `imageright_num` bigint(20) NOT NULL,
+  `complete_num` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `digital_team_shinchoku`
+--
+
+DROP TABLE IF EXISTS `digital_team_shinchoku`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `digital_team_shinchoku` (
+  `id` bigint(20) NOT NULL,
+  `municipality_id` bigint(20) NOT NULL,
+  `barcode_id` bigint(20) NOT NULL,
+  `copyright` int(11) NOT NULL,
+  `imageright` int(11) NOT NULL,
+  `registration_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-06-15 14:32:20
