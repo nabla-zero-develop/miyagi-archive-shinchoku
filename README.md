4d483f8d13eb78a15fa2103b2aba3d5070e4f1e8
Hideo Ohashi <hideld@infoseek.jp>
2014-06-15 16:28:10 +0900

    集計用SQLスクリプトを追加（デジタル化チームはまだ未対応）

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ shinchoku/css/jquery-ui-1.10.4.custom.min.css ... jQuery UI v1.10.4
 shinchoku/js/jquery-1.11.1.min.js ... jQuery v1.11.1  
 shinchoku/js/jquery-ui-1.10.4.custom.min.js ... jQuery UI v1.10.4  
 shinchoku/js/jquery.ui.datepicker-ja.min.js ... Struts2-jQuery(datepicker日本語化用)  
+
+定期実行プログラム
+--------
+# 実行方法
+`mysql -u <ユーザ名> -p<パスワード> < <sqlファイルが存在するディレクトリ>/shinchoku.sql`
+
+# 定期実行設定
+production_program/shinchoku.sqlを任意のディレクトリにコピーし、cronに上記実行方法を追記する
---
+++ production_program/shinchoku.sql
@@ -0,0 +1,24 @@
+INSERT into miyagi_archive_shinchoku.daily_shinchoku
+SELECT
+	now() AS counting_date,
+	1 AS categoryid,
+	holderid,
+	count(holderid) AS content_num,
+	count(if(md_copyright!=0 and kiso_editflag=0 and kihon_editflag=0, 1, null)) as copyright_num,
+	count(if(md_imageright!=0 and kiso_editflag=0 and kihon_editflag=0, 1, null)) as imageright_num,
+	count(if(md_copyright!=0 and md_imageright!=0 and kiso_editflag=0 and kihon_editflag=0, 1, null)) as complete_num
+FROM miyagi_archive_ken.content
+WHERE holderid>=121000
+GROUP BY holderid
+UNION ALL
+SELECT
+	now() AS counting_date,
+	2 AS categoryid,
+	holderid,
+	count(holderid) AS content_num,
+	count(if(md_copyright!=0 and kiso_editflag=0 and kihon_editflag=0, 1, null)) as copyright_num,
+	count(if(md_imageright!=0 and kiso_editflag=0 and kihon_editflag=0, 1, null)) as imageright_num,
+	count(if(md_copyright!=0 and md_imageright!=0 and kiso_editflag=0 and kihon_editflag=0, 1, null)) as complete_num
+FROM miyagi_archive_shichouson.content
+WHERE holderid<990
+GROUP BY holderid;
