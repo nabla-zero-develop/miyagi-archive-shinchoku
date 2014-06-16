458bffca7a023ca6e1c4fcfa900846036449550c
Hideo Ohashi <hideld@infoseek.jp>
2014-06-17 00:42:47 +0900

    デジタル化チームの進捗も集計する

--- production_program/shinchoku.sql
+++ production_program/shinchoku.sql
@@ -1,24 +1,35 @@
-INSERT into miyagi_archive_shinchoku.daily_shinchoku
+INSERT INTO miyagi_archive_shinchoku.daily_shinchoku
 SELECT
-	now() AS shinchoku_date,
+	NOW() AS shinchoku_date,
 	1 AS categoryid,
 	holderid,
-	count(holderid) AS content_num,
-	count(if(md_copyright!=0 and kiso_editflag=0 and kihon_editflag=0, 1, null)) as copyright_num,
-	count(if(md_imageright!=0 and kiso_editflag=0 and kihon_editflag=0, 1, null)) as imageright_num,
-	count(if(md_copyright!=0 and md_imageright!=0 and kiso_editflag=0 and kihon_editflag=0, 1, null)) as complete_num
+	COUNT(holderid) AS content_num,
+	COUNT(IF(md_copyright!=0 AND kiso_editflag=0 AND kihon_editflag=0, 1, NULL)) AS copyright_num,
+	COUNT(IF(md_imageright!=0 AND kiso_editflag=0 AND kihon_editflag=0, 1, NULL)) AS imageright_num,
+	COUNT(IF(md_copyright!=0 AND md_imageright!=0 AND kiso_editflag=0 AND kihon_editflag=0, 1, NULL)) AS complete_num
 FROM miyagi_archive_ken.content
 WHERE holderid>=121000
 GROUP BY holderid
 UNION ALL
 SELECT
-	now() AS shinchoku_date,
+	NOW() AS shinchoku_date,
 	2 AS categoryid,
 	holderid,
-	count(holderid) AS content_num,
-	count(if(md_copyright!=0 and kiso_editflag=0 and kihon_editflag=0, 1, null)) as copyright_num,
-	count(if(md_imageright!=0 and kiso_editflag=0 and kihon_editflag=0, 1, null)) as imageright_num,
-	count(if(md_copyright!=0 and md_imageright!=0 and kiso_editflag=0 and kihon_editflag=0, 1, null)) as complete_num
+	COUNT(holderid) AS content_num,
+	COUNT(IF(md_copyright!=0 AND kiso_editflag=0 AND kihon_editflag=0, 1, NULL)) AS copyright_num,
+	COUNT(IF(md_imageright!=0 AND kiso_editflag=0 AND kihon_editflag=0, 1, NULL)) AS imageright_num,
+	COUNT(IF(md_copyright!=0 AND md_imageright!=0 AND kiso_editflag=0 AND kihon_editflag=0, 1, NULL)) AS complete_num
 FROM miyagi_archive_shichouson.content
 WHERE holderid<990
-GROUP BY holderid;
+GROUP BY holderid
+UNION ALL
+SELECT
+	NOW() AS shinchoku_date,
+	3 AS categoryid,
+	municipality_id AS holderid,
+	COUNT(municipality_id) AS content_num,
+	COUNT(IF(copyright!=0, 1, NULL)) AS copyright_num,
+	COUNT(IF(imageright!=0, 1, NULL)) AS imageright_num,
+	COUNT(IF(copyright!=0 AND imageright!=0, 1, NULL)) AS complete_num
+FROM miyagi_archive_shinchoku.digital_team_shinchoku
+GROUP BY municipality_id;
