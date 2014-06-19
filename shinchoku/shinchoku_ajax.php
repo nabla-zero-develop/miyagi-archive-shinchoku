d90cdba27248d04bd5f849c7cb6f568e15cd1da0
Hideo Ohashi <hideld@infoseek.jp>
2014-06-20 00:23:36 +0900

    当日の日付が指定された場合、現時点のコンテンツテーブルを集計して表示する

--- shinchoku/shinchoku_ajax.php
+++ shinchoku/shinchoku_ajax.php
@@ -20,28 +20,79 @@ $options = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
 );
 
-if ($categoryid == 1) {
-    $holderTable = "miyagi_archive_ken.holder";
-    $joinCondition = "a.holderid=b.id";
-} elseif ($categoryid == 2) {
-    $holderTable = "miyagi_archive_shichouson.holder";
-    $joinCondition = "a.holderid=b.id";
-} elseif ($categoryid == 3) {
-    $holderTable = "miyagi_archive_shichouson.sikucyoson";
-    $joinCondition = "TRUNCATE(a.holderid/10, 0)=b.code";
-} else {
-    exit();
-}
+$dt = new DateTime("+1 day");
+$today = $dt->format('Y-m-d');
 
 $pdo = new PDO($dsn, $db["user"], $db["password"], $options);
 
-$stmt = $pdo->prepare(
-    "SELECT b.name, a.content_num, a.copyright_num, a.imageright_num, a.complete_num, TRUNCATE(a.complete_num*100/a.content_num, 1) AS complete_percent" .
-    " FROM miyagi_archive_shinchoku.daily_shinchoku a JOIN " . $holderTable . " b ON " . $joinCondition .
-    " WHERE a.categoryid=? AND DATE(a.shinchoku_date)=?"
-);
+if (strtotime($shinchokuDate) != strtotime($today)) {
+    if ($categoryid == 1) {
+        $holderTable = "miyagi_archive_ken.holder";
+        $joinCondition = "a.holderid=b.id";
+    } elseif ($categoryid == 2) {
+        $holderTable = "miyagi_archive_shichouson.holder";
+        $joinCondition = "a.holderid=b.id";
+    } elseif ($categoryid == 3) {
+        $holderTable = "miyagi_archive_shichouson.sikucyoson";
+        $joinCondition = "TRUNCATE(a.holderid/10, 0)=b.code";
+    } else {
+        exit();
+    }
 
-$stmt->execute(array($categoryid, $shinchokuDate));
+    $stmt = $pdo->prepare(
+        "SELECT b.name, a.content_num, a.copyright_num, a.imageright_num, a.complete_num, TRUNCATE(a.complete_num*100/a.content_num, 1) AS complete_percent" .
+        " FROM miyagi_archive_shinchoku.daily_shinchoku a JOIN " . $holderTable . " b ON " . $joinCondition .
+        " WHERE a.categoryid=? AND DATE(a.shinchoku_date)=?"
+    );
+
+    $stmt->execute(array($categoryid, $shinchokuDate));
+} else {
+    if ($categoryid == 1) {
+        $sql = <<< SQL
+SELECT
+	b.name,
+	COUNT(holderid) AS content_num,
+	COUNT(IF(md_copyright!=0 AND kiso_editflag=0 AND kihon_editflag=0, 1, NULL)) AS copyright_num,
+	COUNT(IF(md_imageright!=0 AND kiso_editflag=0 AND kihon_editflag=0, 1, NULL)) AS imageright_num,
+	COUNT(IF(md_copyright!=0 AND md_imageright!=0 AND kiso_editflag=0 AND kihon_editflag=0, 1, NULL)) AS complete_num,
+    TRUNCATE(COUNT(IF(md_copyright!=0 AND md_imageright!=0 AND kiso_editflag=0 AND kihon_editflag=0, 1, NULL)) * 100 / COUNT(holderid), 1) AS complete_percent
+FROM miyagi_archive_ken.content JOIN miyagi_archive_ken.holder b ON holderid=b.id
+WHERE holderid>=121000
+GROUP BY holderid;
+SQL;
+    } elseif ($categoryid == 2) {
+        $sql = <<< SQL
+SELECT
+	b.name,
+	COUNT(holderid) AS content_num,
+	COUNT(IF(md_copyright!=0 AND kiso_editflag=0 AND kihon_editflag=0, 1, NULL)) AS copyright_num,
+	COUNT(IF(md_imageright!=0 AND kiso_editflag=0 AND kihon_editflag=0, 1, NULL)) AS imageright_num,
+	COUNT(IF(md_copyright!=0 AND md_imageright!=0 AND kiso_editflag=0 AND kihon_editflag=0, 1, NULL)) AS complete_num,
+    TRUNCATE(COUNT(IF(md_copyright!=0 AND md_imageright!=0 AND kiso_editflag=0 AND kihon_editflag=0, 1, NULL)) * 100 / COUNT(holderid), 1) AS complete_percent
+FROM miyagi_archive_shichouson.content JOIN miyagi_archive_shichouson.holder b ON holderid=b.id
+WHERE holderid<990
+GROUP BY holderid;
+SQL;
+    } elseif ($categoryid == 3) {
+        $sql = <<< SQL
+SELECT
+	b.name,
+	COUNT(municipality_id) AS content_num,
+	COUNT(IF(copyright!=0, 1, NULL)) AS copyright_num,
+	COUNT(IF(imageright!=0, 1, NULL)) AS imageright_num,
+	COUNT(IF(copyright!=0 AND imageright!=0, 1, NULL)) AS complete_num,
+    TRUNCATE(COUNT(IF(copyright!=0 AND imageright!=0, 1, NULL)) * 100 / COUNT(municipality_id), 1) AS complete_percent
+FROM miyagi_archive_shinchoku.digital_team_shinchoku JOIN miyagi_archive_shichouson.sikucyoson b ON TRUNCATE(municipality_id/10, 0)=b.code
+GROUP BY municipality_id;
+SQL;
+    } else {
+        exit();
+    }
+
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->execute();
+}
 
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
